1d4ae6ab87f84d55fb1c9510388656599df2f350
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-18 11:41:33 +0530

    DataTypes

---
+++ 02_dataype.js
@@ -0,0 +1,25 @@
+"use strict";// treatv all js code as newer version
+
+// alert(3+3) // we are  using nodejs, not browser
+//when to use semicolon 
+console.log(3+3)
+//code readibility
+console.log("Shreya")
+let name ="Shreya"
+let age = 20
+let isLoggedIn = false
+//state = null // null is a object 
+
+
+//number => 2 to power
+// bigint =>use for large number use in trading and stocks
+//string => ""
+//null => standalone value
+//undefined =>
+//symbol=>unique
+
+//object
+console.log(typeof "shreya");
+console.log(typeof "null");
+
+
---
+++ basics/01_var.js
@@ -0,0 +1,8 @@
+const account = 144553
+let account_Email ="hitesh@google.com"
+var accountPassword = "12345"
+account_city="Jaipur"
+// prefer not to use var
+//because of iisue in block scope and functional scope
+accountId = 2
+console.log(accountId)
