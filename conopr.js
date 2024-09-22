48beebca8c63ea83a0126ad9487120e670539357
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-09-22 15:05:11 +0530

    Datatype conversion

---
+++ conopr.js
@@ -0,0 +1,24 @@
+// let score = 3
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+       // important notes//
+//0.let score ="33abc"gives output as a number in js but give NAN THAT not a number 
+// 1.if value taken null like
+// 2.let score = null it give output 0
+//3.may be it is temp val 
+//4.if boolen value it give true=>1 , False=>0
+//5.if given undefined give output=> NAN
+
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn)
+// it give output as true justb because int converted to boolean
+
+//let isLoggedIn =""
+//it gives output false
+
+//let isLoggedIn ="hitesh"
+//it give output true
